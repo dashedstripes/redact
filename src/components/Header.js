7703da33dfb57a441637e8bf491705b51121c89c
Adam Gray <adam@pateo.co>
2018-03-27 17:21:07 +0100

    Add a few routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
   render() {
     return (
       <div>
-        Header
+        <ul>
+          <li>
+            <Link to='/'>redact</Link>
+          </li>
+          <li>
+            <Link to='/todo'>todo</Link>
+          </li>
+          <li>
+            <Link to='/about'>about</Link>
+          </li>
+        </ul>
       </div>
     )
   }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react'
 
 import Footer from '../components/Footer'
 import Header from '../components/Header'
-import TodoList from '../components/TodoList'
 
 class Layout extends Component {
   render() {
     return (
       <div>
         <Header />
-        <TodoList />
+        {this.props.children}
         <Footer />
       </div>
     )
